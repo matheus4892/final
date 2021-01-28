820bbf99c98413062445d1ecd42f0a87dd7b0564
matheus4892 <78133017+matheus4892@users.noreply.github.com>
2021-01-28 00:32:35 -0800

    content

---
+++ w.js
@@ -0,0 +1,13 @@
+
+$(document).ready(() => {
+    $("#b2").click(() => {
+        document.getElementById("rowSection").scrollIntoView({behavior:"smooth"})
+        
+       
+    })
+    $("#b3").click(() => {
+        document.getElementById("vid").scrollIntoView({behavior:"smooth"})
+        
+    })
+})
+
